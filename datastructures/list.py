bf33fac49bf8ec47dd34f1c9b7341d5c5ca976dc
sparksflyupwards <saad.jamil.khan@gmail.com>
2020-02-28 14:00:23 -0500

    completed merge list algorithm!

--- datastructures/list.py
+++ datastructures/list.py
@@ -5,14 +5,24 @@ class Node:
 
 
 class NodeList:
-    def __init__(self,val):
-        self.head = Node(val)
-        self.tailNode = None
-
-        self.head.next=self.tailNode
+    
+    def __init__(self,*args):
+        if len(args)>0:
+            if isinstance(args[0], int):
+                self.head = Node(args[0])
+                self.tailNode = None
+                self.head.next=self.tailNode
+        else:
+            self.head = None
+            self.tailNode = None
+           
     def getHeadNode(self):
         return self.head
     def addNode(self,_node):
+        if self.head == None:
+            self.head = _node
+            self.head.next=self.tailNode
+            return
         if not self.tailNode == None:
             temp_node = self.tailNode
             self.tailNode = _node
@@ -59,36 +69,42 @@ def reverseListOld(list):
 def mergeLists(l1,l2):
     l1_head= l1.getHeadNode()
     l2_head= l2.getHeadNode()
-
+    third_list = NodeList()
     while not l1_head == None or not l2_head == None:
         
         if not l1_head == None and not l2_head == None:
             if l1_head.value < l2_head.value:
-                print(l1.head.value)
+                #print(l1_head.value)
+                third_list.addNode(Node(l1_head.value))
                 l1_head = l1_head.next
                 continue
+                
             else:
-                print(l2.head.value)
+                #print(str(l2_head.value))
+                third_list.addNode(Node(l2_head.value))
                 l2_head = l2_head.next
                 continue
+                
 
-            if not l1_head == None:
-                print(l1_head.value)
+        if not l1_head == None:
+                #print(l1_head.value)
+                third_list.addNode(Node(l1_head.value))
                 l1_head = l1_head.next
-                continue
-            if not l2_head == None:
-                print(l2_head.value)
+                
+        if not l2_head == None:
+                #print(l2_head.value)
+                third_list.addNode(Node(l2_head.value))
                 l2_head = l2_head.next
-                continue
+    return third_list
         
 l = NodeList(1)
 l.addNode(Node(2))
 l.addNode(Node(3))
 l.addNode(Node(4))
 l.addNode(Node(5))
-#l.printNodes()
 l2 = NodeList(1)
 l2.addNode(Node(3))
 l2.addNode(Node(44))
 
-mergeLists(l,l2)
+third_list = mergeLists(l,l2)
+third_list.printNodes()
