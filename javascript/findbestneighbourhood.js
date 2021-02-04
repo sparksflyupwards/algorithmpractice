52934605c178e330c3e50a63057e6319b8ebef7d
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-02-04 14:11:21 -0500

    seemingly works but what about the edgecase where there is less than the requested requirements across the whole neighbourhood?

--- javascript/findbestneighbourhood.js
+++ javascript/findbestneighbourhood.js
@@ -1,20 +1,22 @@
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
+
 let neighbourHoods = [
     {"park": false,
-     "gym": true,
+     "gym": false,
     "grocer": false},
     {"park": false,
     "gym": false,
    "grocer": false},
    {"park": false,
-    "gym": true,
-   "grocer": true},
-   {"park": true,
     "gym": false,
    "grocer": false},
    {"park": false,
     "gym": false,
-   "grocer": true},
-   {"park": true,
+   "grocer": false},
+   {"park": false,
+    "gym": false,
+   "grocer": false},
+   {"park": false,
     "gym": false,
    "grocer": false}
 ]
@@ -24,7 +26,47 @@ const requirements = ["gym", "park", "grocer"];
 
 
 findBestNeighbourhood = (neighbourhoods, requirements)=> { 
-    console.log(requirements);
+    let lowestDistance = {distance: null, index: null};
+    let distances = [];
+    for(let i=0; i<neighbourHoods.length; i++){
+        let max_distance_at_location=-1;
+       
+            console.log("goig left at "+i)
+        for(let req=0; req<requirements.length; req++){
+              if(i>0){
+                    //traverse left
+                for(let left = i-1; left>=0; left-- ){
+                    let current_requirement = requirements[req];
+                    if(neighbourHoods[left][current_requirement]==true){
+                        if((i-left)>max_distance_at_location){
+                            max_distance_at_location = i-left;
+                        }
+                    }
+                }
+            }
+            if(i<requirements.length-1){
+                //traverse right
+                for(let right = i+1; right<requirements.length; right++){
+                    let current_requirement = requirements[req];
+                    console.log(neighbourHoods[right] + current_requirement)
+                    if(neighbourHoods[right][current_requirement]==true){
+                        if((right-i)>max_distance_at_location){
+                            max_distance_at_location = right-i;
+                        }
+                    }
+                }
+                }
+            }
+            if(lowestDistance.distance == null || lowestDistance.distance == -1 || lowestDistance.distance > max_distance_at_location){
+                lowestDistance.distance = max_distance_at_location;
+                lowestDistance.index = i;
+            }
+            distances.push({distance:max_distance_at_location, index:i})
+
+        }   
+        return lowestDistance;
+    
 }
 
-findBestNeighbourhood(neighbourHoods, requirements);
+lowest = findBestNeighbourhood(neighbourHoods, requirements);
+console.log(lowest)
