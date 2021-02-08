31294b453fadbb13cd17bbe4924a57df4c79c5b7
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-02-07 23:12:40 -0500

    used promises to build a counter

---
+++ javascript/playingwithpromises.js
@@ -0,0 +1,28 @@
+let p = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("true")
+    },
+    1000)
+    
+})
+let count = 0;
+let counter = ()=>{
+    console.log("Count: "+count)
+    count ++;
+}
+let newPromise = (callback, time)=>{
+        return new Promise((resolve, reject)=>{ 
+            setTimeout( ()=>{
+                resolve(callback())
+            }, time)
+        });
+}
+
+newPromise(counter, 1000)
+.then((data)=>newPromise(counter, 1000))
+.then(()=>newPromise(counter, 1000))
+.then(()=>newPromise(counter, 1000))
+.then(()=>newPromise(counter, 1000))
+.then(()=>newPromise(counter, 1000))
+
+//console.log(newPromise(()=>console.log("ok"), 1000).then((data)=> console.log("then")))
