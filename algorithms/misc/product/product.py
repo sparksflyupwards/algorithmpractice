6946df85e5c1d5762c4734869a5f679ff18b1d13
sparksflyupwards <saad.jamil.khan@gmail.com>
2022-01-11 12:23:14 -0500

    Karatsuba now works for odd numbers as well as numbers of uneven length, this completes the coursera assignment

--- algorithms/misc/product/product.py
+++ algorithms/misc/product/product.py
@@ -1,3 +1,4 @@
+
 class Product(object):
     __productType = "basic"
     def __init__(self, productType):
@@ -32,41 +33,81 @@ class Product(object):
 
         firstHalf = str(array[0])
         secondHalf = str(array[1])
- 
 
         sizeFirstNum = len(firstHalf)
-        a = firstHalf[ : len(firstHalf)//2]
-        b = firstHalf[len(firstHalf)//2 : ]
+        sizeSecondNum = len(secondHalf)
+        zerosAdded = 0
+        altered = False
 
-        c = secondHalf[ : len(secondHalf)//2]
-        d = secondHalf[len(secondHalf)//2 : ]
+        #   if we have an odd number or the numbers are uneven in length then even them out
+        #   and remember divide the answer by the requiste amount and recursively call the function
+        #   else just continue with the function
 
-        a = int(a)
-        b = int(b)
-        c = int(c)
-        d = int(d)
+        if sizeFirstNum % 2 != 0 or sizeSecondNum % 2 != 0:
+            firstHalf = int(firstHalf) * 10
+            secondHalf = int(secondHalf) * 10
+            zerosAdded += 2
+            altered = True
 
+        if sizeFirstNum > sizeSecondNum:
+            sizeDiff = sizeFirstNum - sizeSecondNum
+            secondHalf = secondHalf * pow(10, sizeDiff)
+            zerosAdded += sizeDiff
+            altered =  True
 
-        if(len(str(a)) > 4):
-            highProductNums = [a,c]
-            highProduct = self.karatsuba(highProductNums)
-            lowProductNums = [b,d]
-            lowProduct = self.karatsuba(lowProductNums)
-        else:
-            highProduct = a  * c
-            lowProduct = b * d
-
+        if sizeFirstNum < sizeSecondNum:
+            sizeDiff = sizeSecondNum - sizeFirstNum
+            firstHalf = firstHalf * pow(10, sizeDiff)
+            zerosAdded += sizeDiff
+            altered =  True
+       
         
-        sumProduct = a + b
-        sumProduct *= c + d
-        difference = sumProduct - highProduct - lowProduct
-        
-        product = highProduct * pow(10,sizeFirstNum) + lowProduct  + difference * pow(10,sizeFirstNum/2)
+        if altered:
+            return self.karatsuba([str(firstHalf), str(secondHalf)]) / pow(10, zerosAdded)
 
+        if not altered:
+            a = firstHalf[ : len(firstHalf)//2]
+            b = firstHalf[len(firstHalf)//2 : ]
 
-        print("step: "+firstHalf + " * " + secondHalf + " = " + str(product))
-        print("step: "+firstHalf + " * " + secondHalf + " should = " + str(int(firstHalf)*int(secondHalf)))
-        return product
+            c = secondHalf[ : len(secondHalf)//2]
+            d = secondHalf[len(secondHalf)//2 : ]
+            
+         
+            
+            a = int(a)
+            b = int(b)
+            c = int(c)
+            d = int(d)
+            print("this iteration a b c d")
+            print(a)
+            print(b)
+            print(c)
+            print(d)
+
+            if(len(str(firstHalf)) > 4 and len(str(firstHalf))/2%2==0):
+                highProductNums = [a,c]
+                highProduct = self.karatsuba(highProductNums)
+                lowProductNums = [b,d]
+                lowProduct = self.karatsuba(lowProductNums)
+            else:
+                highProduct = a  * c
+                lowProduct = b * d
+
+            
+            sumProduct = a + b
+            sumProduct *= c + d
+            difference = sumProduct - highProduct - lowProduct
+            product = highProduct * pow(10,sizeFirstNum) + lowProduct  + pow(10,sizeFirstNum / 2) * difference
+            product = int(product)
+
+            print("step: "+firstHalf + " * " + secondHalf + " = " + str(product))
+            print("cort: "+firstHalf + " * " + secondHalf + " = " + str(int(firstHalf)*int(secondHalf)))
+
+            #   if answers of the karatsuba and the built in multiplcation methods
+            #   do not match this message gets printed
+            if str(product) != str(int(firstHalf)*int(secondHalf)):
+                print("THINGS WENT VERY WRONG HERE\n")
+            return product
 
 
 import sys      
